f99b6c46d715cf14f50dbb81e7b5ad5f2ae6320b
Alexander Fischer <fischer.alexander@web.de>
2020-05-03 14:18:53 +0200

    updated to version 20.5.2

--- qownnotes/tools/chocolateyInstall.ps1
+++ qownnotes/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 $ErrorActionPreference = 'Stop'
 $packageName    = 'qownnotes'
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url            = 'https://github.com/pbek/QOwnNotes/releases/download/windows-b5612/QOwnNotes.zip' 
-$checksum       = 'D2BC4C664A09FCD51436986EB5F19CF01ECD556EB4A0BC05990146768F308800'
+$url            = 'https://github.com/pbek/QOwnNotes/releases/download/windows-b5617/QOwnNotes.zip' 
+$checksum       = '86CA7562453D13F57EC43C7B8DB0272C6C958377886AEB42DCA3F338208E5C55'
 $url64          = $url
 $checksum64     = $checksum
 $checksumType   = 'sha256' 
