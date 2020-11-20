9dff2ff1ced484dba4c1b4ade0af5db2d2a5cb92
TheCakeIsNaOH <TheCakeIsNaOH@gmail.com>
2020-11-20 15:32:04 -0600

    Qownnotes include zip in nupkg

--- qownnotes/tools/chocolateyUninstall.ps1
+++ qownnotes/tools/chocolateyUninstall.ps1
@@ -1,4 +1,4 @@
-$programsDir = [System.Environment]::GetFolderPath('Programs')
+﻿$programsDir = [System.Environment]::GetFolderPath('Programs')
 
 if ((Test-Path -Path "$programsDir\QOwnNotes.lnk") -Or (Test-Path -Path "$programsDir\QOwnNotes (Portable).lnk")) {
   Remove-Item -Force -ea 0 "$programsDir\QOwnNotes.lnk"
