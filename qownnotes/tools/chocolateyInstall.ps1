949a917e6a3c722f5b010f257beed680b09a619f
Alexander Fischer <fischer.alexander@web.de>
2023-02-05 21:25:00 +0100

    refreshed for version 23.2.0

--- qownnotes/tools/chocolateyInstall.ps1
+++ qownnotes/tools/chocolateyInstall.ps1
@@ -1,8 +1,12 @@
 ﻿$ErrorActionPreference = 'Stop'
+$packageName    = 'qownnotes'
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url            = 'https://github.com/pbek/QOwnNotes/releases/download/v23.2.0/QOwnNotes.zip'
+$checksum       = '79323449015713E94FF29D47FEFAA788372E201D5F938436EE185B891AA80466'
+
 
 $packageArgs = @{
-    PackageName  = $env:ChocolateyPackageName
+    PackageName  = $packageName
     Destination  = $toolsDir
     FileFullPath = Join-Path $toolsDir 'QOwnNotes.zip'
 }
