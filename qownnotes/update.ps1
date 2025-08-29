a9df8bd8957d09e20b6c4464bb7d0b82f34c10bf
Alexander Fischer <fischer.alexander@web.de>
2025-08-29 08:23:31 +0200

    Korrigiere Regex-Muster in der au_SearchReplace-Funktion zur Unterstützung von Checksumme, ChecksumType und Version

--- qownnotes/update.ps1
+++ qownnotes/update.ps1
@@ -3,10 +3,10 @@ Import-Module Chocolatey-AU
 function global:au_SearchReplace {
     @{
         ".\tools\chocolateyInstall.ps1" = @{
-            "(?i)(^\s*(\$)url\s*=\s*)('.*')"  = "`$1'$($Latest.URL32)'"
-            "(?i)(^\s*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
-            "(?i)(^\s*checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
-            "(?i)(^\s*version\s*=\s*)('.*')"  = "`$1'$($Latest.Version)'"
+            "(?i)(\s*(\$)url\s*=\s*)('.*')"  = "`$1'$($Latest.URL32)'"
+            "(?i)(\s*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+            "(?i)(\s*checksumType\s*=\s*)('.*')" = "`$1'$($Latest.ChecksumType32)'"
+            "(?i)(\s*version\s*=\s*)('.*')"  = "`$1'$($Latest.Version)'"
         }
         ".\legal\VERIFICATION.txt" = @{
             "(?i)(\s+x32:).*"            = "`$1 $($Latest.URL32)"
