18e6319bc359d6aeaa0b4a12b51296a7eabf4db9
Rico <d-Rickyy-b@users.noreply.github.com>
2026-01-13 00:01:10 +0100

    fix: update duplicati install script to use msi
    fixes #11

--- duplicati/tools/chocolateyinstall.ps1
+++ duplicati/tools/chocolateyinstall.ps1
@@ -2,10 +2,10 @@
 $packageName    = 'duplicati'
 $version        = '2.2.0.3'
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url            = 'https://github.com/duplicati/duplicati/releases/download/v2.2.0.3_stable_2026-01-06/duplicati-2.2.0.3_stable_2026-01-06-win-x86-gui.zip' 
-$checksum       = 'EDBCDB9BC13D7FE915AA61670EE3D67AFD468DD4675741D53828ECD7A51AAD6A'
-$url64          = 'https://github.com/duplicati/duplicati/releases/download/v2.2.0.3_stable_2026-01-06/duplicati-2.2.0.3_stable_2026-01-06-win-x64-gui.zip' 
-$checksum64     = '162C63F111C7AC4FF57DED6A2D67FBE7B127BB173FBF589013A01A7C13A9587F'
+$url            = 'https://github.com/duplicati/duplicati/releases/download/v2.2.0.3_stable_2026-01-06/duplicati-2.2.0.3_stable_2026-01-06-win-x86-gui.msi' 
+$checksum       = 'D58FEA6FA7FA3918E7421CB98EAE2BDCE5C99167A6F94CA90EFC2C9A4FE0D43A'
+$url64          = 'https://github.com/duplicati/duplicati/releases/download/v2.2.0.3_stable_2026-01-06/duplicati-2.2.0.3_stable_2026-01-06-win-x64-gui.msi' 
+$checksum64     = '3B7B7A9B930D74D3E735E67CEA4032911B2D2DC10664F85862411D45390936AD'
 
 $packageArgs = @{
   packageName    = $packageName
